b3c6264381a7fa631ca273ec975dc446e2bd9bf8
Camilo Delgado <camiloadh99@gmail.com>
2022-11-19 12:48:21 -0500

    feat: creacion de hook para media query

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useWindowSize } from "./Hooks/useMediaQuery ";
+import { diviceType } from "./utilities/findDiviceType";
 
 function App() {
+  const windowSize = useWindowSize();
+  const diviceTypeString = diviceType(windowSize?.width);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="center-items">
+        <h1>
+          Width x height: {windowSize?.width} x {windowSize?.height}
+        </h1>
+        <h2>Type: {diviceTypeString} </h2>
+      </div>
     </div>
   );
 }
